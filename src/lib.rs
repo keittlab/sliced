364c3fae76bc984dfb028ab2cf7f3cd1ee65660d
Timothy H. Keitt <tkeitt@utexas.edu>
2023-05-22 23:25:49 -0500

    add insert

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use std::{
     ops::{Index, IndexMut, Range},
+    ptr,
     slice::{Chunks, ChunksMut, Iter, IterMut},
 };
 
@@ -38,6 +39,18 @@ where
         assert_eq!(other.segment_len, self.segment_len);
         self.storage.append(&mut other.storage)
     }
+    pub fn insert(&mut self, index: usize, segment: &[T]) {
+        debug_assert!(index < self.len());
+        assert_eq!(segment.len(), self.segment_len);
+        let orig_last_index = self.last_index();
+        self.storage.extend_from_within(self.storage_range_last());
+        if index < orig_last_index {
+            let src = self.storage_range_range(index, orig_last_index - 1);
+            let dst = self.storage_begin(index + 1);
+            self.storage.copy_within(src, dst);
+        }
+        unsafe { self.overwrite(index, segment) }
+    }
     pub fn push(&mut self, segment: &[T]) {
         assert_eq!(segment.len(), self.segment_len);
         self.storage.extend_from_slice(segment)
@@ -50,33 +63,31 @@ where
         self.storage.get_mut(range)
     }
     pub fn swap_remove(&mut self, index: usize) -> Vec<T> {
-        assert!(index < self.len());
-        let last_index = self.len() - 1;
-        if index != last_index {
+        debug_assert!(index < self.len());
+        if index != self.last_index() {
             self.storage_range(index)
-                .zip(self.storage_range(last_index))
+                .zip(self.storage_range_last())
                 .for_each(|(i, j)| self.storage.swap(i, j))
         }
         self.storage
-            .drain(self.storage_range(last_index))
+            .drain(self.storage_range_last())
             .as_slice()
             .into()
     }
-    pub fn overwrite_remove(&mut self, index: usize) {
-        assert!(index < self.len());
-        let last_index = self.len() - 1;
-        if index != last_index {
-            let source_range = self.storage_range(last_index);
-            self.storage
-                .copy_within(source_range, index * self.segment_len)
+    pub fn swap_truncate(&mut self, index: usize) {
+        debug_assert!(index < self.len());
+        if index != self.last_index() {
+            let src = self.storage_range_last();
+            let dst = self.storage_begin(index);
+            self.storage.copy_within(src, dst)
         }
         self.storage.truncate(self.storage.len() - self.segment_len)
     }
     pub fn swap_insert(&mut self, index: usize, segment: &[T]) {
-        assert!(index < self.len());
+        debug_assert!(index < self.len());
         assert_eq!(segment.len(), self.segment_len);
         self.storage.extend_from_within(self.storage_range(index));
-        // finish
+        unsafe { self.overwrite(index, segment) }
     }
     pub fn iter(&self) -> Chunks<'_, T> {
         self.storage.chunks(self.segment_len)
@@ -96,11 +107,35 @@ where
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
+    fn storage_begin(&self, index: usize) -> usize {
+        debug_assert!(index < self.len());
+        index * self.segment_len
+    }
+    fn storage_end(&self, index: usize) -> usize {
+        debug_assert!(index < self.len());
+        self.storage_begin(index) + self.segment_len
+    }
     fn storage_range(&self, index: usize) -> Range<usize> {
-        assert!(index < self.len());
-        let start = index * self.segment_len;
-        let end = start + self.segment_len;
-        start..end
+        debug_assert!(index < self.len());
+        self.storage_begin(index)..self.storage_end(index)
+    }
+    fn storage_range_range(&self, begin: usize, end: usize) -> Range<usize> {
+        self.storage_begin(begin)..self.storage_end(end)
+    }
+    fn storage_range_last(&self) -> Range<usize> {
+        self.storage_range(self.last_index())
+    }
+    fn last_index(&self) -> usize {
+        self.len() - 1
+    }
+    unsafe fn overwrite(&mut self, index: usize, segment: &[T]) {
+        debug_assert!(index < self.len());
+        debug_assert_eq!(self.segment_len, segment.len());
+        ptr::copy(
+            segment.as_ptr(),
+            self.storage.as_mut_ptr().add(self.storage_begin(index)),
+            self.segment_len,
+        )
     }
 }
 
@@ -153,6 +188,12 @@ mod tests {
         assert_eq!(&a[1], &[7, 8, 9]);
         a.append(&mut vecvec!(&[3, 6, 9]));
         assert_eq!(&a[2], &[3, 6, 9]);
+        a.insert(1, &[3, 2, 1]);
+        assert_eq!(&a[3], &[3, 6, 9]);
+        assert_eq!(&a[1], &[3, 2, 1]);
+        a.swap_insert(1, &[2, 2, 2]);
+        assert_eq!(&a[4], &[3, 2, 1]);
+        assert_eq!(&a[1], &[2, 2, 2]);
         let mut v: VecVec<i32> = VecVec::new(3);
         assert_eq!(v.len(), 0);
         v.push(&[1, 2, 3]);
@@ -184,14 +225,14 @@ mod tests {
         assert_eq!(w.get(0).unwrap()[2], 0);
         w.push(&[10, 20, 30, 40, 50]);
         w.push(&[100, 200, 300, 400, 500]);
-        w.overwrite_remove(0);
+        w.swap_truncate(0);
         assert_eq!(w.len(), 2);
         assert_eq!(&w[0], &[100, 200, 300, 400, 500]);
         assert_eq!(&w[1], &[10, 20, 30, 40, 50]);
-        w.overwrite_remove(1);
+        w.swap_truncate(1);
         assert_eq!(w.len(), 1);
         assert_eq!(&w[0], &[100, 200, 300, 400, 500]);
-        w.overwrite_remove(0);
+        w.swap_truncate(0);
         assert_eq!(w.len(), 0);
         assert!(w.is_empty());
     }
